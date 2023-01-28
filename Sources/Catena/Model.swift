8b0db60ea92c0fb4223f647a387f930156c0212a
Jordan Kay <jordan@fleuronic.com>
2023-01-27 23:20:51 -0500

    Remove foreign keys

--- Sources/Catena/Model.swift
+++ Sources/Catena/Model.swift
@@ -5,19 +5,12 @@ import PersistDB
 
 public protocol Model: PersistDB.Model where ID: Decodable {
 	var valueSet: ValueSet<Self> { get }
-	var foreignKeys: ForeignKeys { get }
 
 	static var idKeyPath: KeyPath<Self, ID> { get }
 }
 
 // MARK: -
 public extension Model {
-	typealias ForeignKeys = [PartialKeyPath<Self>: PartialKeyPath<Self>]
-
-	var foreignKeys: ForeignKeys {
-		[:]
-	}
-
 	var identifiedValueSet: ValueSet<Self> {
 		valueSet.update(with: [Self.idKeyPath == id])
 	}
