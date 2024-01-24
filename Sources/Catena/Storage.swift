e8e5c2af38147a70934e937ab99d0bc2ba765e19
Jordan Kay <jordan@fleuronic.com>
2024-01-24 09:25:48 -0500

    Add fetch by ID

--- Sources/Catena/Storage.swift
+++ Sources/Catena/Storage.swift
@@ -9,6 +9,7 @@ public protocol Storage {
 	func insert<Model: Catena.Model>(_ model: Model) async -> Result<Model.ID, StorageError>
 	func insert<Model: Catena.Model>(_ models: [Model]) async -> Result<[Model.ID], StorageError>
 	func fetch<Model: Catena.Model>(where predicate: Predicate<Model>?) async -> Result<[Model.ID], StorageError>
+	func fetch<Fields: Catena.Fields>(_ fields: Fields.Type, with id: Fields.Model.ID) async -> Result<[Fields], StorageError>
 	func fetch<Fields: Catena.Fields>(_ fields: Fields.Type, where: Predicate<Fields.Model>?) async -> Result<[Fields], StorageError>
 	func update<Model: Catena.Model>(_ valueSet: ValueSet<Model>, with id: Model.ID) async -> Result<Model.ID, StorageError>
 	func update<Model: Catena.Model>(_ valueSet: ValueSet<Model>, where predicate: Predicate<Model>?) async -> Result<[Model.ID], StorageError>
