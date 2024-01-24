e4625838f0d9d5824a870f9017860c1d5020e1d2
Jordan Kay <jordan@fleuronic.com>
2024-01-24 09:25:16 -0500

    Update formatting

--- Sources/Catena/Storage.swift
+++ Sources/Catena/Storage.swift
@@ -12,7 +12,7 @@ public protocol Storage {
 	func fetch<Fields: Catena.Fields>(_ fields: Fields.Type, where: Predicate<Fields.Model>?) async -> Result<[Fields], StorageError>
 	func update<Model: Catena.Model>(_ valueSet: ValueSet<Model>, with id: Model.ID) async -> Result<Model.ID, StorageError>
 	func update<Model: Catena.Model>(_ valueSet: ValueSet<Model>, where predicate: Predicate<Model>?) async -> Result<[Model.ID], StorageError>
-    func delete<Model: Catena.Model>(_ type: Model.Type, with id: Model.ID) async -> Result<Model.ID?, StorageError>
+	func delete<Model: Catena.Model>(_ type: Model.Type, with id: Model.ID) async -> Result<Model.ID?, StorageError>
 	func delete<Model: Catena.Model>(_ type: Model.Type, with ids: [Model.ID]) async -> Result<[Model.ID], StorageError>
 	func delete<Model: Catena.Model>(_ type: Model.Type, where predicate: Predicate<Model>?) async -> Result<[Model.ID], StorageError>
 }
