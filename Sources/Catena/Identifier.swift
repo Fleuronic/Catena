0621c86e9e05113d684271a62e7aa331d7ff717f
Jordan Kay <jordan@fleuronic.com>
2024-02-09 11:09:35 -0500

    Support all identifiers

--- Sources/Catena/Identifier.swift
+++ Sources/Catena/Identifier.swift
@@ -1,19 +1,54 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
 import struct Identity.Identifier
-import struct Foundation.UUID
 import struct Schemata.Value
+import struct Schemata.AnyValue
+import struct Foundation.UUID
 import protocol Schemata.ModelValue
 import protocol Schemata.AnyModelValue
 
-extension Identifier: AnyModelValue where Value.RawIdentifier == UUID {}
+public extension Identifier where Value.RawIdentifier == UUID {
+	static var random: Self { .init(rawValue: .init()) }
+}
 
 // MARK: -
-extension Identifier: ModelValue where Value.RawIdentifier == UUID {
-	public static var value: Schemata.Value<String, Self> {
-		UUID.value.bimap(
+extension Identifier: AnyModelValue where Value.RawIdentifier: ModelValue & StringEncodable {
+	public static var anyValue: AnyValue {
+		.init(
+			String.value.bimap(
+				decode: Value.RawIdentifier.encode,
+				encode: \.description
+			)
+		)
+	}
+}
+
+// MARK: -
+extension Identifier: ModelValue where Value.RawIdentifier: ModelValue & StringEncodable {
+	public static var value: Schemata.Value<Value.RawIdentifier.Encoded, Self> {
+		Value.RawIdentifier.value.bimap(
 			decode: Self.init(rawValue:),
 			encode: \.rawValue
 		)
 	}
+}
+
+// MARK: -
+public protocol StringEncodable: CustomStringConvertible {
+	static func encode(with string: String) -> Self
+}
+
+// MARK: -
+extension Int: StringEncodable {
+	public static func encode(with string: String) -> Self { .init(string)! }
+}
+
+// MARK: -
+extension String: StringEncodable {
+	public static func encode(with string: String) -> Self { string }
+}
+
+// MARK: -
+extension UUID: StringEncodable {
+	public static func encode(with string: String) -> Self { .init(uuidString: string)! }
 }
