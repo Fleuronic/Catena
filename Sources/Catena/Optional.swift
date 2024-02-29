6a43a2e7fec0aada0176919bcfe42fc380cd17e0
Jordan Kay <jordan@fleuronic.com>
2024-02-29 15:19:01 -0500

    Use if expressions in Optional

--- Sources/Catena/Optional.swift
+++ Sources/Catena/Optional.swift
@@ -1,24 +1,15 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
 public extension Optional {
-    func asyncMap<T>(transform: (Wrapped) async -> T) async -> T? {
-        if let result = self {
-            return await transform(result)
-        }
-        return nil
-    }
-
-	func asyncFlatMap<T>(transform: (Wrapped) async -> T?) async -> T? {
-		if let result = self {
-			return await transform(result)
-		}
-		return nil
+	func asyncMap<T>(transform: (Wrapped) async -> T) async -> T? {
+		if let result = self { await transform(result) } else { nil }
 	}
 
-    func asyncMapNil(defaultValue: () async -> Wrapped) async -> Wrapped {
-        if let result = self {
-            return result
-        }
-        return await defaultValue()
-    }
+	func asyncFlatMap<T>(transform: (Wrapped) async -> T?) async -> T? {
+		if let result = self { await transform(result) } else { nil }
+	}
+
+	func asyncMapNil(defaultValue: () async -> Wrapped) async -> Wrapped {
+		if let result = self { result } else { await defaultValue() }
+	}
 }
