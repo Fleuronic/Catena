dde4a23d33da3c73e17d45e62f951cc00941468b
Jordan Kay <jordan@fleuronic.com>
2024-05-15 14:06:44 -0400

    Remove dependencies

--- Package.swift
+++ Package.swift
@@ -17,21 +17,7 @@ let package = Package(
 			targets: ["Catena"]
 		)
 	],
-	dependencies: [
-		.package(url: "https://github.com/JohnSundell/Identity", from: "0.1.0"),
-		.package(url: "https://github.com/LeonardoCardoso/InitMacro.git", branch: "main"),
-		.package(url: "https://github.com/almazrafi/DictionaryCoder.git", from: "1.2.0"),
-		.package(url: "https://github.com/FelixHerrmann/swift-multipart-formdata.git", from: "1.3.0")
-	],
 	targets: [
-		.target(
-			name: "Catena",
-			dependencies: [
-				"Identity",
-				"InitMacro",
-				"DictionaryCoder",
-				.productItem(name: "MultipartFormData", package: "swift-multipart-formdata")
-			]
-		)
+		.target(name: "Catena")
 	]
 )
