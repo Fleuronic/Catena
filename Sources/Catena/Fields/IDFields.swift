e3b7bb006ed41c08451b862ea4030b6850e380d3
Jordan Kay <jordan@fleuronic.com>
2024-09-06 19:02:33 -0400

    Fix warnings

--- Sources/Catena/Fields/IDFields.swift
+++ Sources/Catena/Fields/IDFields.swift
@@ -1,10 +1,11 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
-import MemberwiseInit
-
 import protocol Identity.Identifiable
 
-@MemberwiseInit(.public)
 public struct IDFields<Model: Identifiable & Sendable>: Fields, Hashable where Model.ID: Hashable & Sendable, Model.RawIdentifier: Sendable {
 	public let id: Model.ID
+
+	@Sendable public init(id: Model.ID) {
+		self.id = id
+	}
 }
--- Sources/Catena/Fields/IDListFields.swift
+++ Sources/Catena/Fields/IDListFields.swift
@@ -1,10 +1,11 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
-import MemberwiseInit
-
 import protocol Identity.Identifiable
 
-@MemberwiseInit(.public)
 public struct IDListFields<Model: Identifiable & Sendable>: Fields, Sendable where Model.ID: Sendable {
 	public let ids: [Model.ID]
+
+	@Sendable public init(ids: [Model.ID]) {
+		self.ids = ids
+	}
 }
