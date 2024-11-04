3bc41fcf573c9560fe39dac2f422f66fb8512d39
Jordan Kay <jordan@fleuronic.com>
2024-11-04 17:22:12 -0500

    Update submodules

--- Sources/Catena/Fields/UndocumentedFields.swift
+++
@@ -1,9 +0,0 @@
-// Copyright © Fleuronic LLC. All rights reserved.
-
-public protocol UndocumentedFields: Fields {
-	var undocumentedFields: [PartialKeyPath<Self>: Bool] { get }
-}
-
-public extension UndocumentedFields {
-	var undocumentedFields: [PartialKeyPath<Self>: Bool] { [:] }
-}
