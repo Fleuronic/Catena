573e08588ed48d194b3eae45471d4d0a0961c66c
Jordan Kay <jordan@fleuronic.com>
2024-11-15 13:25:52 -0500

    Add result providing protocol

--- Sources/Catena/Result.swift
+++ Sources/Catena/Result.swift
@@ -1,5 +1,14 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
+public protocol ResultProviding {
+	associatedtype Error: Swift.Error
+
+	typealias Results<Resource> = Result<[Resource], Error>
+	typealias SingleResult<Resource> = Result<Resource, Error>
+	typealias ImpossibleResult = Never
+	typealias NoResult = Result<Void, Error>
+}
+
 public extension Result {
 	func map<NewSuccess>(_ transform: (Success) async -> NewSuccess) async -> Result<NewSuccess, Failure> {
 		switch self {
