a0282ce7a5739366e5ca9035330b08ab95b9aff3
Jordan Kay <jordan@fleuronic.com>
2024-11-21 09:00:07 -0500

    Update formatting

--- Sources/Catena/Nullable.swift
+++ Sources/Catena/Nullable.swift
@@ -6,6 +6,7 @@ public protocol Nullable {
 	var wrapped: Wrapped? { get }
 }
 
+// MARK: -
 extension Optional: Nullable {
 	public var wrapped: Wrapped? { self }
 }
--- Sources/Catena/Scoped.swift
+++ Sources/Catena/Scoped.swift
@@ -4,14 +4,17 @@ public protocol Scoped<Fields> {
 	associatedtype Fields: Catena.Fields
 }
 
+// MARK: -
 extension Result: Scoped where Success: Scoped {
 	public typealias Fields = Success.Fields
 }
 
+// MARK: -
 extension Array: Scoped where Element: Scoped {
 	public typealias Fields = Element.Fields
 }
 
+// MARK: -
 extension Optional: Scoped where Wrapped: Scoped {
 	public typealias Fields = Wrapped.Fields
 }
