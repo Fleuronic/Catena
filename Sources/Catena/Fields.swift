058bd11412e34f9b4be4f0e8667e52a268902e3b
Jordan Kay <jordan@fleuronic.com>
2025-01-28 13:56:20 -0500

    Add associated model type to fields

--- Sources/Catena/Fields.swift
+++ Sources/Catena/Fields.swift
@@ -1,3 +1,5 @@
 // Copyright © Fleuronic LLC. All rights reserved.
 
-public protocol Fields: Scoped, Sendable where Self.Fields == Self {}
+public protocol Fields: Scoped, Sendable where Self.Fields == Self {
+	associatedtype Model
+}
