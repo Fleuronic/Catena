a8b0a3168eac3d3c102da409993f03356b800885
Jordan Kay <jordan@fleuronic.com>
2025-06-13 10:05:11 -0400

    Update output

---
+++ Sources/Catena/Output.swift
@@ -0,0 +1,26 @@
+// Copyright © Fleuronic LLC. All rights reserved.
+
+public protocol Output<Success> {
+	associatedtype Success
+	associatedtype Failure: Error
+
+	var results: Results { get }
+}
+
+// MARK: -
+public extension Output {
+	typealias Results = AsyncStream<Result<Success, Failure>>
+}
+
+// MARK: -
+extension Result: Output {}
+
+extension Result where Success: Sendable {
+	// MARK: WorkerOutput
+	public var results: Results {
+		.init { continuation in
+			continuation.yield(self)
+			continuation.finish()
+		}
+	}
+}
