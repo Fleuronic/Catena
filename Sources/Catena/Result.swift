0352a88ac735c234973c5e61e7cd18ec73b103d8
Jordan Kay <jordan@fleuronic.com>
2025-06-16 23:33:59 -0400

    Update method naming

--- Sources/Catena/Result.swift
+++ Sources/Catena/Result.swift
@@ -20,7 +20,7 @@ public extension Result {
 		}
 	}
 
-	func flatMap<NewSuccess>(_ transform: (Success) async -> Result<NewSuccess, Failure>) async -> Result<NewSuccess, Failure> {
+	func asyncFlatMap<NewSuccess>(_ transform: (Success) async -> Result<NewSuccess, Failure>) async -> Result<NewSuccess, Failure> {
 		switch self {
 		case let .success(value): await transform(value)
 		case let .failure(error): .failure(error)
