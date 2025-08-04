099204d06647496bc3ff378b3d379d921a63fb80
Jordan Kay <jordan@fleuronic.com>
2025-08-04 18:29:53 -0400

    Add optional async map

--- Sources/Catena/Nullable.swift
+++ Sources/Catena/Nullable.swift
@@ -6,6 +6,16 @@ public protocol Nullable {
 	var wrapped: Wrapped? { get }
 }
 
+public extension Optional {
+	func asyncMap<T>(_ transform: (Wrapped) async throws -> T) async rethrows -> T? {
+		if let value = self {
+			try await transform(value)
+		} else {
+			nil
+		}
+	}
+}
+
 // MARK: -
 extension Optional: Nullable {
 	public var wrapped: Wrapped? { self }
